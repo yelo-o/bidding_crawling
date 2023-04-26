b1544d049266df7345cccb80f8549fdadb362ed4
flyordig <flyordig@gmail.com>
2023-04-26 16:49:53 +0900

    파일 정리

--- 입찰운영사업팀.py
+++ 입찰운영사업팀.py
@@ -12,14 +12,10 @@ now = datetime.now() # 오늘 날짜 불러오기
 cr_today = now.strftime("%Y-%m-%d") # 2023-04-26 같은 형식으로 오늘 날짜 불러오기
 
 full_wm = [cr_today, '월드와이드메모리'] + extract_wm() + [None]
-# print("월드와이드메모리 전처리 완료")
-# print(full_wm)
+
 full_nm = [cr_today, '나노메모리'] + extract_nm() + [None]
-# print("나노메모리 전처리 완료")
-# print(full_nm)
+
 full_danawa = [cr_today, '다나와'] + extract_danawa() + [None]
-# print("다나와 전처리 완료")
-# print(full_danawa)
 
 save_to_file(cr_today,full_wm,full_nm,full_danawa)
 print("데이터 csv 파일 저장 완료")
